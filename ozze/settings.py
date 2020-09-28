4aea764c757942c528304444f6dac1d75e2d122c
SpalpIN <64166547+SpalpIN@users.noreply.github.com>
2020-09-28 18:54:21 +0300

    delete a secret key

--- ozze/settings.py
+++ ozze/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't1o2a)vxsl#2ap5wnp0m2*v+&q!fp^!qog2kyh=7tt18!i^v1k'
+SECRET_KEY = '******'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
